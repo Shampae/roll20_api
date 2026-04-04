82b5b0d4a6101594388292abcabdab6098d23560
Shampae <hinu206@gmail.com>
2026-04-04 23:52:22 +0900

    Update change_profile.js

--- change_profile.js
+++ change_profile.js
@@ -1,27 +1,23 @@
 // https://github.com/Shampae/roll20_api/blob/main/change_profile.js
 
-// 명령어 사용법: !pf -시트상의 캐릭터 이름 -표정1
-// 예시: !pf -얀 -기본
-// 시작 전 준비: 캐릭터별 이미지변수생성, 코드삽입
+//명령어 사용법: !pf -저널명 -표정명
+//시작 전 준비: 이미지 변수화, 변수 적용하는 case문 작성
+//프로필에 사용할 이미지 전부 변수로 저장
 
-// !!! 주석에 # 처리 해둔 부분을 GM이 직접 수정해야 합니다 !!!
-// 예시변수들은 지워도 무관합니다. 주석처리 되어있기 때문에 그대로 두어도 영향을 끼치진 않습니다.
+/*
+리스트 선택 매크로
 
+!pf -저널명
+-?{선택|
+선택지,표정명|
+기본,기본|
+...}
 
-// #
-// 프로필에 사용할 이미지 전부 변수로 저장. 캐릭터 별로 전부 생성해 주세요
-// 만약 동작하지 않는다면, 링크의 max.png? 부분을 thumb.png? 로 변경해 주세요
-const 캐릭터변수명 = {
-    "표정1": "링크",
-    "표정2": "링크",
-    "표정3": "링크"
+*/
+
+const charactor = {
+    "표정명": "롤20서버 업로드한 프사 링크넣고 중간부분 /thumb.png? 로 수정"
 }
-
-// 예시코드
-/*const yann = {
-    "기본": "https://files.d20.io/images/473767125/hFPejpqL--i6N3RneFAX3g/thumb.png?1769791538",
-    "웃음": "https://files.d20.io/images/476616935/GJUYszzrK5gE0_aA3Kd8Qw/thumb.png?1771599228"
-}*/
 
 on("chat:message", function(msg) {
     if (msg.type == "api") {
@@ -38,31 +34,27 @@ on("chat:message", function(msg) {
                     sendChat("ERROR", "/w GM 이름오류");
                     return;
                 }
-                
                 let name = split[1];
                 switch (name) {
-                    // #
-                    // 아래의 코드를 적용할 전원으로 만들어 주세요
-                    case "시트상의 캐릭터 이름":
-                        cha[0].set('avatar', #캐릭터변수명[index]);
+                    /* 캐릭터별로 생성
+                    case "저널명":
+                        cha[0].set('avatar', 캐릭터 변수명[index]);
                         sendChat("", "/desc <div style='display: block; height: 0px; margin: -35px 0 -15px 0; padding: 0; font-size: 0px; line-height: 0px; overflow: hidden; visibility: hidden;'>.</div>");
                         break;
-                    case "시트상의 캐릭터 이름(두번째 캐릭터)":
-                        cha[0].set('avatar', #캐릭터변수명(두번째 캐릭터)[index]);
-                        sendChat("", "/desc <div style='display: block; height: 0px; margin: -35px 0 -15px 0; padding: 0; font-size: 0px; line-height: 0px; overflow: hidden; visibility: hidden;'>.</div>");
-                        break;
+                    case ""
+                    */
                     
                     // 예시코드
-                    /*case "얀" :
-                        cha[0].set('avatar', yann[index]);
+                    case "저널명" :
+                        cha[0].set('avatar', charactor[index]);
                         sendChat("", "/desc <div style='display: block; height: 0px; margin: -35px 0 -15px 0; padding: 0; font-size: 0px; line-height: 0px; overflow: hidden; visibility: hidden;'>.</div>");
                         break;
-                    */
+                        
                 }
             } catch (err) {
                 sendChat("ERROR", "/w gm 오류 발생: " + err.message);
             }
-            
         }
     }
+});
 });
